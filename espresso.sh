756d11285e245bb899ba7133943a41732594a3c6
Uncle Bogdan <85982863+freshe4qa@users.noreply.github.com>
2022-06-17 12:58:40 +0300

    Create espresso.sh

---
+++ espresso.sh
@@ -0,0 +1,28 @@
+# Logo
+
+echo -e '\e[40m\e[91m'
+echo -e '  ____                  _                    '
+echo -e ' / ___|_ __ _   _ _ __ | |_ ___  _ __        '
+echo -e '| |   |  __| | | |  _ \| __/ _ \|  _ \       '
+echo -e '| |___| |  | |_| | |_) | || (_) | | | |      '
+echo -e ' \____|_|   \__  |  __/ \__\___/|_| |_|      '
+echo -e '            |___/|_|                         '
+echo -e '    _                 _                      '
+echo -e '   / \   ___ __ _  __| | ___ _ __ ___  _   _ '
+echo -e '  / _ \ / __/ _  |/ _  |/ _ \  _   _ \| | | |'
+echo -e ' / ___ \ (_| (_| | (_| |  __/ | | | | | |_| |'
+echo -e '/_/   \_\___\__ _|\__ _|\___|_| |_| |_|\__  |'
+echo -e '                                       |___/ '
+echo -e '\e[0m'
+
+sleep 2
+
+sudo apt update && sudo apt upgrade -y
+
+sudo snap install docker
+
+curl https://www.espressosys.com/cape/docker-compose.yaml --output docker-compose.yaml
+
+docker-compose pull
+
+docker-compose up
